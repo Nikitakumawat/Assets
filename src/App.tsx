d788083b5d7179cf9a2afe256b82b7c608a89a09
Nikita Kumawat <35212192+Nikitakumawat@users.noreply.github.com>
2023-03-07 14:05:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import "./App.css";
 import Card from "./components/Card";
 import data from "./data.json";
 
+interface cryptoCurrency {
+  type: string;
+  currentPrice: number;
+  totalValue: number;
+  icon: string;
+  change: number;
+  currency?: string;
+  pairs: string[];
+}
+
 const App: FC = () => {
   return (
     <div className="bg-[#14172B] h-full">
       <p className="text-white font-bold p-2">Trending assets</p>
       <div className="flex flex-wrap">
-        {data.cryptoCurrency.map((currentValue, index) => (
+        {data.cryptoCurrency.map((currentValue: cryptoCurrency, index: number) => (
           <Card
             key={index}
             type={currentValue.type}
